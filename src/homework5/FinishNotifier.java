26b1781b39d0d555476bcdd1c05604844d8a1546
AndreyGeekBrains <lostserial5@gmail.com>
2020-06-15 11:29:52 +0300

    HomeWork5

---
+++ src/homework5/FinishNotifier.java
@@ -0,0 +1,20 @@
+package homework5;
+
+import java.util.concurrent.CountDownLatch;
+
+public class FinishNotifier {
+    private final CountDownLatch countDownLatch;
+    public FinishNotifier(int carsCount) {
+        this.countDownLatch = new CountDownLatch(carsCount);
+    }
+    public void waitWhileAllCarsFinished() {
+        try {
+            countDownLatch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+    public void notifyAboutFinished() {
+        countDownLatch.countDown();
+    }
+}
