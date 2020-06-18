6550fa0e14a14836027020b37326bf2164073829
AndreyGeekBrains <lostserial5@gmail.com>
2020-06-18 15:58:34 +0300

    HomeWork6

---
+++ src/homework6/Main2.java
@@ -0,0 +1,12 @@
+package homework6;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        Main2Task1.class,
+        Main2Task2.class,
+})
+public class Main2 {
+}
